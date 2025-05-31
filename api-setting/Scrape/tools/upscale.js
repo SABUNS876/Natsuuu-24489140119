8b8c76ca6892d857bc28e79a143343a8d882575d
SABUNS876 <149647540+SABUNS876@users.noreply.github.com>
2025-05-31 22:11:05 +0700

    Update upscale.js

--- api-setting/Scrape/tools/upscale.js
+++ api-setting/Scrape/tools/upscale.js
@@ -1,25 +1,23 @@
-import axios from 'axios'
-import cheerio from 'cheerio'
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 async function imgHd(url, scales) {
- let data = axios(`https://toolsapi.spyne.ai/api/forward`, {
-    method: "post",
-    data: {
-      image_url: url,
-      scale: scales,
-      save_params: {
-        extension: ".png",
-        quality: 100,
-      },
-    },
+  let data = await axios.post('https://toolsapi.spyne.ai/api/forward', {
+    image_url: url,
+    scale: scales,
+    save_params: {
+      extension: ".png",
+      quality: 100,
+    }
+  }, {
     headers: {
       "content-type": "application/json",
       accept: "*/*",
-    },
-  })
-  return data
+    }
+  });
+  return data;
 }
 
-export {
-imgHd
-}
+module.exports = {
+  imgHd
+};
