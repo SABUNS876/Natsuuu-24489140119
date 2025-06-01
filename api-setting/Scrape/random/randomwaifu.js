49c73de4f3538fe839feed28bdf7a98e21412152
SABUNS876 <149647540+SABUNS876@users.noreply.github.com>
2025-06-01 12:41:52 +0700

    Update randomwaifu.js

--- api-setting/Scrape/random/randomwaifu.js
+++ api-setting/Scrape/random/randomwaifu.js
@@ -2,8 +2,19 @@ const axios = require('axios');
 
 async function getWaifu() {
   try {
-    const response = await axios.get('https://api.waifu.pics/sfw/waifu');
-    return response.data.url;
+    // Dapatkan URL gambar
+    const urlResponse = await axios.get('https://api.waifu.pics/sfw/waifu');
+    const imageUrl = urlResponse.data.url;
+    
+    // Ambil gambar sebagai buffer
+    const imageResponse = await axios.get(imageUrl, {
+      responseType: 'arraybuffer'
+    });
+    
+    return {
+      buffer: Buffer.from(imageResponse.data, 'binary'),
+      url: imageUrl
+    };
   } catch (error) {
     console.error('Gagal mengambil gambar:', error);
     throw error;
