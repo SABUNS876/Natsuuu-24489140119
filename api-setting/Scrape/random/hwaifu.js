81eb207316768fe7570576189e0aeb8253c539e7
SABUNS876 <149647540+SABUNS876@users.noreply.github.com>
2025-06-05 16:40:34 +0700

    Create hwaifu.js

---
+++ api-setting/Scrape/random/hwaifu.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+async function getWaifu() {
+  try {
+    // Dapatkan URL gambar terlebih dahulu
+    const urlResponse = await axios.get('https://api.waifu.pics/nsfw/waifu');
+    const imageUrl = urlResponse.data.url;
+    
+    // Ambil gambar sebagai buffer
+    const imageResponse = await axios.get(imageUrl, {
+      responseType: 'arraybuffer'
+    });
+    
+    // Kembalikan sebagai buffer dengan content type
+    return {
+      imageBuffer: Buffer.from(imageResponse.data, 'binary'),
+      contentType: imageResponse.headers['content-type']
+    };
+    
+    // Atau bisa juga langsung return Buffer saja:
+    // return Buffer.from(imageResponse.data, 'binary');
+  } catch (error) {
+    console.error('Error getting waifu image:', error);
+    throw error;
+  }
+}
+
+module.exports = getWaifu;
