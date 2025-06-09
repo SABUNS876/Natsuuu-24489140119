b8fab7f2a2ca256bad7deeb65875a4670c6f1f11
SABUNS876 <149647540+SABUNS876@users.noreply.github.com>
2025-06-09 20:40:03 +0700

    Update savepin.js

--- api-setting/Scrape/search/savepin.js
+++ api-setting/Scrape/search/savepin.js
@@ -1,35 +1,25 @@
-const cheerio = require("cheerio");
-const axios = require('axios');
+const fetch = require('node-fetch');
 
-async function savePin(url) {
-	try {
-		const response = await axios.get(`https://www.savepin.app/download.php?url=${encodeURIComponent(url)}&lang=en&type=redirect`);
-		const html = response.data;
-		const $ = cheerio.load(html);
-
-		let results = [];
-		$('td.video-quality').each((index, element) => {
-			const type = $(element).text().trim();
-			const format = $(element).next().text().trim();
-			const downloadLinkElement = $(element).nextAll().find('#submiturl').attr('href');
-			if (downloadLinkElement) {
-				let downloadLink = downloadLinkElement;
-				if (downloadLink.startsWith('force-save.php?url=')) {
-					downloadLink = decodeURIComponent(downloadLink.replace('force-save.php?url=', ''));
-				}
-				results.push({ type, format, downloadLink });
-			}
-		});
-		const title = $('h1').text().trim();
-
-		return {
-			title,
-			results
-		};
-	} catch (error) {
-		console.error("Error:", error.response ? error.response.data : error.message);
-		return { success: false, message: error.message };
-	}
+const pint = async (query) => {
+    const response = await fetch("https://www.pinterest.com/resource/BaseSearchResource/get/?data=" + encodeURIComponent('{"options":{"query":"' + encodeURIComponent(query) + '"}}'), {
+        "headers": {
+            "screen-dpr": "4",
+            "x-pinterest-pws-handler": "www/search/[scope].js",
+        },
+        "method": "post"
+    });
+    
+    if (!response.ok) throw new Error(`Error ${response.status} ${response.statusText}`);
+    const rhl = response.headers.get("Link");
+    if (!rhl) throw new Error(`Search results for "${query}" are empty`);
+    
+    const links = [...rhl.matchAll(/<(.*?)>/gm)].map(v => v[1]);
+    return links;
 };
 
-module.exports = savePin;
+// Example usage
+pint("furina")
+    .then(console.log)
+    .catch(err => console.log(err.message));
+
+module.exports = pint;
